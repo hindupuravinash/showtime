de3d7c4b3dd238b5f34b371eb1eb0fc8ab068a96
Avinash Hindupur <hindupuravinash@gmail.com>
2015-07-05 23:55:05 +0530

    Refactor typos and PersonActivity

--- app/src/main/java/in/nash/cram/ui/PersonActivity.java
+++ app/src/main/java/in/nash/cram/ui/PersonActivity.java
@@ -3,16 +3,13 @@ package in.nash.cram.ui;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 
-import in.nash.cram.R;
-
 /**
- * Created by Avinash Hindupur on 04/07/15.
+ * Created by Avinash Hindupur on 05/07/15.
  */
-public class AboutActivity extends AppCompatActivity{
+public class PersonActivity  extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_about);
     }
 }
