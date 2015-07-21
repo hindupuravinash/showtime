b832b7710db6fbc05bf658dc2ae7c74f7dc1b5de
Avinash Hindupur <hindupuravinash@gmail.com>
2015-07-21 15:42:32 +0530

    Display Version in About

--- app/src/main/java/in/nash/cram/ui/AboutActivity.java
+++ app/src/main/java/in/nash/cram/ui/AboutActivity.java
@@ -2,8 +2,10 @@ package in.nash.cram.ui;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 import in.nash.cram.R;
+import in.nash.cram.utils.AppUtils;
 
 /**
  * Created by Avinash Hindupur on 04/07/15.
@@ -14,5 +16,8 @@ public class AboutActivity extends AppCompatActivity{
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_about);
+
+        TextView versionText = (TextView) findViewById(R.id.version);
+        versionText.setText("Version: " + AppUtils.getVersionName());
     }
 }
---
+++ app/src/main/java/in/nash/cram/utils/AppUtils.java
@@ -0,0 +1,13 @@
+package in.nash.cram.utils;
+
+import in.nash.cram.BuildConfig;
+
+/**
+ * Created by avinash on 7/21/15.
+ */
+public class AppUtils {
+
+    public static String getVersionName() {
+        return BuildConfig.VERSION_NAME + "." + BuildConfig.VERSION_CODE;
+    }
+}
