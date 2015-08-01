74870e6fc59303aafdaa430dbad5230d72efda55
Avinash Hindupur <hindupuravinash@gmail.com>
2015-08-01 23:04:14 +0530

    Movie and Person services

--- app/src/main/java/in/nash/cram/network/MoviesService.java
+++ app/src/main/java/in/nash/cram/network/MoviesService.java
@@ -1,7 +1,73 @@
 package in.nash.cram.network;
 
+import com.google.gson.annotations.SerializedName;
+
+import java.util.ArrayList;
+
+import in.nash.cram.model.Movie;
+import in.nash.cram.model.Person;
+import in.nash.cram.model.Review;
+import retrofit.http.GET;
+import retrofit.http.Path;
+
 /**
  * Created by avinash on 7/20/15.
  */
-public class MoviesService {
+public interface MoviesService {
+
+    @GET("/movie/top_rated")
+    MovieResponse fetchTopMovies();
+
+    @GET("/movie/popular")
+    MovieResponse fetchPopularMovies();
+
+    @GET("/movie/upcoming")
+    MovieResponse fetchUpcomingMovies();
+
+    @GET("/movie/now_playing")
+    MovieResponse fetchNowPlayingMovies();
+
+    @GET("/movie/{id}/similar")
+    MovieResponse fetchSimilarMovies(@Path("id") String id);
+
+    @GET("/movie/{id}/credits")
+    CreditResponse fetchMovieCredits(@Path("id") String id);
+
+    @GET("/movie/{id}/reviews")
+    ReviewResponse fetchMovieReviews(@Path("id") String id);
+
+    @GET("/movie/{id}")
+    Movie fetchMovieDetails(@Path("id") String id);
+
+    public static class ReviewResponse {
+        @SerializedName("id")
+        public String id;
+
+        @SerializedName("page")
+        public int page;
+
+        @SerializedName("results")
+        public ArrayList<Review> mReviews;
+
+        @SerializedName("total_pages")
+        public int totalPages;
+
+        @SerializedName("total_results")
+        public int totalResults;
+    }
+
+    public class CreditResponse {
+        @SerializedName("cast")
+        public ArrayList<Person> mCast;
+
+        @SerializedName("crew")
+        public ArrayList<Person> mCrew;
+    }
+
+    public class MovieResponse {
+
+        @SerializedName("results")
+        public ArrayList<Movie> mMovies;
+
+    }
 }
--- app/src/main/java/in/nash/cram/network/PersonService.java
+++ app/src/main/java/in/nash/cram/network/PersonService.java
@@ -1,7 +1,15 @@
 package in.nash.cram.network;
 
+import in.nash.cram.model.Person;
+import retrofit.http.GET;
+import retrofit.http.Path;
+
 /**
  * Created by avinash on 7/31/15.
  */
-public class PersonService {
+public interface PersonService {
+
+    @GET("/person/{id}")
+    Person fetchPersonDetails(@Path("id") String id);
+
 }
