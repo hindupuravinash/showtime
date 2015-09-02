47a8561fd5960c7e2fb17a8cdb82ef606642b1a5
Avinash Hindupur <hindupuravinash@gmail.com>
2015-09-03 00:28:10 +0530

    Feedback activity

---
+++ app/src/main/java/in/nash/showtime/ui/view/impl/FeedbackActivity.java
@@ -0,0 +1,72 @@
+package in.nash.showtime.ui.view.impl;
+
+import android.app.Activity;
+import android.content.Intent;
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.widget.EditText;
+
+import in.nash.showtime.R;
+import in.nash.showtime.utils.AppUtils;
+
+/**
+ * Created by avinash on 9/3/15.
+ */
+public class FeedbackActivity extends AppCompatActivity {
+
+    Toolbar mToolbar;
+
+    EditText mFeedbackView;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_feedback);
+
+        mFeedbackView = (EditText) findViewById(R.id.feedback);
+        mToolbar = (Toolbar) findViewById(R.id.toolbar);
+
+        setSupportActionBar(mToolbar);
+        getSupportActionBar().setTitle("Feedback");
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+    }
+
+    public static void navigateTo(Activity fromActivity) {
+        AppUtils.navigateToActivitySimple(fromActivity, FeedbackActivity.class);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        getMenuInflater().inflate(R.menu.menu_feedback, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        if (id == R.id.send) {
+
+            String feedbackText = mFeedbackView.getText().toString().trim();
+            Intent intent = new Intent(Intent.ACTION_SEND);
+            intent.setType("plain/text");
+            intent.putExtra(Intent.EXTRA_EMAIL, new String[]{"howdy@nash.in"});
+            intent.putExtra(Intent.EXTRA_SUBJECT, "Feedback for Showtime Android");
+            intent.putExtra(Intent.EXTRA_TEXT, feedbackText);
+            startActivity(Intent.createChooser(intent, ""));
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+}
