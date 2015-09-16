6bc4f8ef6327b5748586758ac8a04e8c9cb013af
Avinash Hindupur <hindupuravinash@gmail.com>
2015-09-16 21:26:31 +0530

    Person Activity with webview implementation

--- app/src/main/java/in/nash/showtime/ui/view/impl/PersonActivity.java
+++ app/src/main/java/in/nash/showtime/ui/view/impl/PersonActivity.java
@@ -1,43 +1,115 @@
 package in.nash.showtime.ui.view.impl;
 
+import android.app.Activity;
 import android.content.Intent;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+import android.view.Window;
+import android.webkit.WebChromeClient;
+import android.webkit.WebView;
+import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 import in.nash.showtime.R;
-import in.nash.showtime.model.Person;
-import in.nash.showtime.ui.presenter.IPersonDetailPresenter;
-import in.nash.showtime.ui.presenter.PresenterFactory;
-import in.nash.showtime.ui.view.IPersonDetailView;
 
 /**
  * Created by Avinash Hindupur on 05/07/15.
  */
-public class PersonActivity extends AppCompatActivity implements IPersonDetailView {
+public class PersonActivity extends AppCompatActivity {
+    
+    private WebView mWebView;
+    private String mUrl;
+    private Toolbar toolbar;
+    private ProgressBar mProgress;
 
-    private String mId;
-
-    @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        this.getWindow().requestFeature(Window.FEATURE_PROGRESS);
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_person);
 
-        Intent intent = getIntent();
-        Bundle extras = intent.getExtras();
-        mId = extras.getString("id");
+        mWebView = (WebView) findViewById(R.id.webview);
+        toolbar = (Toolbar) findViewById(R.id.toolbar);
+        mProgress = (ProgressBar) findViewById(R.id.progress_bar);
 
-        initPresenter();
-    }
+        if (savedInstanceState != null) {
+            ((WebView) findViewById(R.id.webview)).restoreState(savedInstanceState);
+        }
 
-    private void initPresenter() {
+        Bundle extras = getIntent().getExtras();
+        mUrl = extras.getString("url");
 
-        IPersonDetailPresenter personDetailPresenter = PresenterFactory.createPersonDetailPresenter(this);
-        personDetailPresenter.fetchPerson(mId);
+
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        mWebView.setVisibility(View.GONE);
+        mProgress.setVisibility(View.VISIBLE);
+
+        mWebView.setWebViewClient(new CareWebViewClient());
+        mWebView.getSettings().setJavaScriptEnabled(true);
+        mWebView.getSettings().setDomStorageEnabled(true);
+
+        if (savedInstanceState == null) {
+            mWebView.loadUrl(mUrl);
+        }
+
+        mWebView.setWebChromeClient(new WebChromeClient() {
+            public void onProgressChanged(WebView view, int loadProgress) {
+                mProgress.setVisibility(View.VISIBLE);
+                mWebView.setVisibility(View.GONE);
+                if (loadProgress == 100) {
+                    mProgress.setVisibility(View.GONE);
+                    mWebView.setVisibility(View.VISIBLE);
+
+                }
+            }
+        });
     }
 
     @Override
-    public void setPerson(Person person) {
-        //TODO: Setup Person
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+
+        if (id == android.R.id.home) {
+            onBackPressed();
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
     }
 
+    public static void navigateTo(Activity fromActivity, String url) {
+        Intent intent = new Intent(fromActivity, PersonActivity.class);
+        intent.putExtra("url", url);
+        fromActivity.startActivity(intent);
+    }
+
+    private class CareWebViewClient extends WebViewClient {
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return true;
+        }
+
+        @Override
+        public void onPageFinished(WebView view, String url) {
+        }
+    }
+
+    @Override
+    protected void onSaveInstanceState(Bundle outState) {
+        super.onSaveInstanceState(outState);
+        mWebView.saveState(outState);
+    }
+
+    @Override
+    protected void onRestoreInstanceState(Bundle savedInstanceState) {
+        super.onRestoreInstanceState(savedInstanceState);
+        mWebView.restoreState(savedInstanceState);
+    }
 }
