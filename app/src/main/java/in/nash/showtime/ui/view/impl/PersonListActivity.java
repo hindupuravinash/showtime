30afdf1a0def9150ca6f3baf9f7a262784407dfb
Avinash Hindupur <hindupuravinash@gmail.com>
2016-01-02 23:57:08 +0530

    Tiny tweaks to layouts and onUp navigation from person list

--- app/src/main/java/in/nash/showtime/ui/view/impl/PersonListActivity.java
+++ app/src/main/java/in/nash/showtime/ui/view/impl/PersonListActivity.java
@@ -2,9 +2,9 @@ package in.nash.showtime.ui.view.impl;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
 
 import in.nash.showtime.R;
 
@@ -29,11 +29,18 @@ public class PersonListActivity extends AppCompatActivity {
         getSupportFragmentManager().beginTransaction().add(R.id.list_container, personListFragment, personListFragment.TAG).commit();
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        final ActionBar ab = getSupportActionBar();
-        ab.setHomeAsUpIndicator(R.drawable.ic_menu);
-        ab.setDisplayHomeAsUpEnabled(true);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        switch (item.getItemId()) {
+            case android.R.id.home:
+                finish();
+                return true;
+        }
+        return super.onOptionsItemSelected(item);
     }
 
 }
